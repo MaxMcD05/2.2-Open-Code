4e11f826eafb5a0a175da475ad925cc070536b6c
MaxMcD05 <mm8564a@student.american.edu>
2023-01-22 21:07:11 -0500

    "wordbank"

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const randingFunc = {
     number: getRandomNumber,
     symbol: getRandomSymbol,
 }
+
+function getRandomLetter() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 162);
+}
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+}
+
