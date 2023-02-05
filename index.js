2d33581d87f6e987ceb8b1368654c24e1446a2a7
MaxMcD05 <mm8564a@student.american.edu>
2023-02-05 16:21:53 -0500

    For()

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const generate1 = document.getElementById('generate');
 
-const randingFunc = {
+const passwordTypes = {
     letter: getRandomLetter,
     number: getRandomNumber,
     symbol: getRandomSymbol,
-}
-//note fromCharCode - 
+};
+
 function getRandomLetter() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 162);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+
 function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
+    const symbols = '!@#$%^&(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() symbols.length)];
 }
 
 generatePassword.addEventListener('click', () => {
@@ -23,7 +24,10 @@ generatePassword.addEventListener('click', () => {
     const passwordLength = 0;
     const passwordTypesArr = Object.values(passwordTypes);
 
-}
+    for (let i = 0; i < passwordLength; i++) {
+        const passwordType = passwordTypesArr[Math.floor(Math.random() * passwordTypesArr.length)];
+        password += passwordType();
+    }
 
-console.log(password);
+    console.log(password);
 });
